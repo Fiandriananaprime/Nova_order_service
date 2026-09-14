27aecdc2d5500dcb5a26ef6312719a5627cb554f
Fiandriananaprime <hei.fiandrianana@gmail.com>
2026-09-14 11:35:00 +0300

    chore: initilizing project configuration

---
+++ src/main/java/org/example/nova_order_service/NovaOrderServiceApplication.java
@@ -0,0 +1,13 @@
+package org.example.nova_order_service;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class NovaOrderServiceApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(NovaOrderServiceApplication.class, args);
+    }
+
+}
